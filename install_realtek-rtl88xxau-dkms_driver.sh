2f3397bf102213dd158b0936eb07ea1f51addcd4
jinouchi <my.unique.screen.name@gmail.com>
2022-09-17 19:09:37 -0600

    Update install_realtek-rtl88xxau-dkms_driver.sh

--- install_realtek-rtl88xxau-dkms_driver.sh
+++ install_realtek-rtl88xxau-dkms_driver.sh
@@ -1,2 +1,9 @@
-sudo apt update                                 # Update repos
-sudo apt install realtek-rtl88xxau-dkms -y      # Install driver
+sudo apt update                                      # Update repos
+sudo apt install dkms realtek-rtl88xxau-dkms -y      # Install driver
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+git clone https://github.com/aircrack-ng/rtl8812au.git
+cd rtl8812au
+make
+sudo make install
+sudo reboot 
