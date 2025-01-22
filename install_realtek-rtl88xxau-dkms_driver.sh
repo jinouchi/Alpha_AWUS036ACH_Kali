c0fea79ae8eda84c244f2fe8e15f8819f1630f97
jinouchi <my.unique.screen.name@gmail.com>
2025-01-22 11:34:37 -0700

    added linux headers dependency for newer versions of Kali

--- install_realtek-rtl88xxau-dkms_driver.sh
+++ install_realtek-rtl88xxau-dkms_driver.sh
@@ -1,7 +1,7 @@
 sudo apt update                                      # Update repos
-sudo apt install dkms realtek-rtl88xxau-dkms -y      # Install driver
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
+sudo apt install dkms realtek-rtl88xxau-dkms install linux-headers-$(uname -r) -y  # install driver and dependencies
 git clone https://github.com/aircrack-ng/rtl8812au.git
 cd rtl8812au
 make
